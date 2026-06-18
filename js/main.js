aaffb56e4609a81f5462f01bcb5a17e72779d9e4
nao270 <nn8.ss27@gmail.com>
2026-06-18 20:43:01 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,104 +1,44 @@
-const TPL_ROWS = 0.1;
-const PAD_T = 0.2;
-const PAD_L = 0.2;
-const PAD_R = 0.1;
+import { Part } from './part.js';
 
-const canvasElm = document.createElement('canvas');
-const statusElm = document.querySelector('#status');
-const dstElm = document.querySelector('#dst')
+const TPL_HEIGHT = 0.1;
+const SCAN_X = 0.2;
+const SCAN_Y = 0.23;
+const SCAN_WIDTH = 0.2;
 
-
-async function loadImg(file) {
-  const imgElm = new Image();
-  const url = URL.createObjectURL(file);
-  imgElm.src = url;
-  await imgElm.decode();
-  const img = cv.imread(imgElm);
-  URL.revokeObjectURL(url);
-  imgElm.remove();
-  return img;
-}
+const canvas = document.createElement('canvas');
+const imgInput = document.querySelector('#img-input');
+const saveButton = document.querySelector('#save-button');
+const dstWrapper = document.querySelector('#dst-wrapper');
+let fileName;
 
 
-function normalize(img, baseCols) {
-  const src = img.clone();
-  const scale = baseCols / src.cols;
-  cv.resize(src, img, new cv.Size(0, 0), scale, scale, cv.INTER_LINEAR);
-  src.delete();
-}
+function scanParts(parts) {
+  let prevPart;
 
-
-function preprocess(img, dst, rect) {
-  const roi = img.roi(rect);
-  cv.cvtColor(roi, dst, cv.COLOR_RGBA2GRAY);
-  roi.delete();
-}
-
-
-function updateTpl(img, tpl, padL, scanCols) {
-  const tplRows = Math.round(img.rows * TPL_ROWS);
-  const rect = new cv.Rect(padL, img.rows - tplRows, scanCols, tplRows);
-  preprocess(img, tpl, rect);
-}
-
-
-function updateTgt(img, tgt, padT, padL, scanCols) {
-  const rect = new cv.Rect(padL, padT, scanCols, img.rows - padT);
-  preprocess(img, tgt, rect);
-}
-
-
-function detectPartRows(tgt, tpl) {
-  const result = new cv.Mat();
-  cv.matchTemplate(tgt, tpl, result, cv.TM_CCOEFF_NORMED);
-  const mml = cv.minMaxLoc(result);
-  const matchY = mml.maxLoc.y;
-  result.delete();
-  return tgt.rows - matchY - tpl.rows;
-}
-
-
-function combineImgs(imgs) {
-  const baseCols = imgs[0].cols;
-  const padL = Math.round(baseCols * PAD_L);
-  const scanCols = Math.round(baseCols * (1 - PAD_L - PAD_R));
-  const lastIndex = imgs.length - 1;
-
-  const mv = new cv.MatVector();
-  const tgt = new cv.Mat();
-  const tpl = new cv.Mat();
-
-  for (const [i, img] of imgs.entries()) {
-    if (i === 0) {
-      mv.push_back(img);
-    } else {
-      if (img.cols !== baseCols) {
-        normalize(img, baseCols);
-      }
-      const padT = Math.round(img.rows * PAD_T);
-      updateTgt(img, tgt, padT, padL, scanCols);
-      const partRows = detectPartRows(tgt, tpl);
-
-      if (partRows > 0) {
-        const rect = new cv.Rect(0, img.rows - partRows, img.cols, partRows);
-        const part = img.roi(rect);
-        mv.push_back(part);
-        part.delete();
-      }
-    }
-
-    if (i !== lastIndex) {
-      updateTpl(img, tpl, padL, scanCols);
-    }
-    img.delete();
+  for (const part of parts) {
+    part.initParams(SCAN_X, TPL_HEIGHT);
+    part.initCtx();
+    part.detectHeight(prevPart);
+    prevPart = part;
   }
+}
 
-  tgt.delete();
-  tpl.delete();
-  const dst = new cv.Mat();
-  cv.vconcat(mv, dst);
-  mv.delete();
-  return dst;
+
+function drawDst(parts) {
+  const height = parts.reduce((total, part) => total + part.height, 0);
+  const ctx = canvas.getContext('2d');
+  canvas.width = Part.baseW;
+  canvas.height = height;
+
+  let startY = height;
+
+  for (const part of parts.reverse()) {
+    startY -= part.srcH;
+    if (part.height > 0) {
+      ctx.drawImage(part.src, 0, startY, Part.baseW, part.srcH);
+    }
+    startY += part.srcH - part.height;
+  }
 }
 
 
@@ -111,44 +51,47 @@ function makeFileName(file) {
 }
 
 
-function createSaveButton(fileName) {
-  const buttonElm = document.createElement('button');
-  buttonElm.textContent = '保存';
-  buttonElm.addEventListener('click', () => {
-    canvasElm.toBlob((blob) => {
-      const a = document.createElement('a');
-      a.href = URL.createObjectURL(blob);
-      a.download = fileName;
-      a.click();
-      setTimeout(() => URL.revokeObjectURL(a.href), 1000);
-    });
+function saveDst() {
+  canvas.toBlob((blob) => {
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = fileName;
+    a.click();
+    setTimeout(() => URL.revokeObjectURL(a.href), 1000);
   });
-  statusElm.textContent = '';
-  statusElm.appendChild(buttonElm);
 }
 
 
-async function main(obj) {
+async function onChangeImgFiles(obj) {
   const files = Array.from(obj.files);
 
   if (files.length) {
-    statusElm.replaceChildren();
-    statusElm.textContent = '処理中…';
-
     try {
-      files.sort((a, b) => a.name.localeCompare(b.name, undefined, {numeric: true}));
-      const imgs = await Promise.all(files.map(async file => await loadImg(file)));
-      const dst = combineImgs(imgs);
+      saveButton.disabled = true;
+      saveButton.textContent = '処理中…';
 
-      cv.imshow(canvasElm, dst);
-      dstElm.appendChild(canvasElm);
-      const fileName = makeFileName(files[0]);
-      createSaveButton(fileName);
-      dst.delete();
-    } catch (err) {
-      canvasElm.remove();
-      statusElm.textContent = `エラーが発生しました。`;
+      files.sort((a, b) => a.name.localeCompare(b.name, undefined, {numeric: true}));
+      const parts = await Promise.all(files.map(async file => await new Part(file)));
+
+      const baseW = parts[0].src.naturalWidth;
+      Part.initStaticParams(baseW, SCAN_Y, SCAN_WIDTH);
+
+      scanParts(parts);
+      drawDst(parts);
+      dstWrapper.appendChild(canvas);
+
+      fileName = makeFileName(files[0]);
+      saveButton.disabled = false;
+      saveButton.textContent = '保存';
+    }
+    catch (err) {
+      canvas.remove();
+      saveButton.textContent = 'エラー発生';
       console.error(err);
     }
   }
 }
+
+
+imgInput.addEventListener('change', (e) => onChangeImgFiles(e.target));
+saveButton.addEventListener('click', saveDst);
